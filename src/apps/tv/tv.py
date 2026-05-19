657a1ad4b84bd92c77be131685c347aa5938d7cd
verbiiyo <dyllanmccreary@gmail.com>
2026-05-19 18:08:21 +0900

    this is what the api for our game of life should look like in python.

--- src/apps/tv/tv.py
+++ src/apps/tv/tv.py
@@ -14,14 +14,34 @@ class TVApp(xos.Application):
         self.randomize_kernel()
 
     def randomize_frame(self):
-        xos.random.uniform_fill(self.frame.tensor, 0.0, 255.0)
-    
+        xos.random.uniform_fill(self.frame.tensor, 0.0, 1.0)
+        self.binarize_frame()
+
+    def binarize_frame(self):
+        mask = self.frame.tensor > 0.5
+        self.frame.tensor[mask] = 255
+        self.frame.tensor[~mask] = 0
+
     def randomize_kernel(self):
-        self.kernel = xos.random.uniform(0.001, 1.001, shape=(3, 3, 3), dtype=xos.float32)
+        # self.kernel = xos.random.uniform(0.001, 1.001, shape=(3, 3, 3), dtype=xos.float32)
+        self.kernel = xos.tensor([[1, 1, 1], [1, 0, 1], [1, 1, 1]])
 
     def tick(self):
         # convolution tv will convolve the random frame
-        xos.ops.convolve(self.frame.tensor, self.kernel, inplace=True)
+        neighbor_counts = xos.ops.convolve(self.frame.tensor, self.kernel, inplace=False, padding="same").to(xos.uint8)
+
+        state = self.frame.tensor
+
+        one_cells = state == 1
+        zero_cells = state == 0
+
+        # product rules
+        state[one_cells and neighbor_counts < 2] = 0                                # only if the original cell was 1
+        state[one_cells and neighbor_counts == 2 or neighbor_counts == 3] = 1       # only if the original cell was 1
+        state[one_cells and neighbor_counts > 3] = 0                                # only if the original cell was 1
+        state[zero_cells and neighbor_counts == 3] = 1                              # only if the original cell was 0
+
+        self.binarize_frame()
         print(self.frame.tensor.device, self.frame.tensor.shape, self.fps)
 
     def on_screen_size_change(self, width, height):
